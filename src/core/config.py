5ec6bf5ea965652443c3970c92c2e3e6a3067ed0
TeemotioBB <r66vtrhwkw@privaterelay.appleid.com>
2026-05-01 14:13:03 -0300

    Update config.py

--- src/core/config.py
+++ src/core/config.py
@@ -1,1 +1,15 @@
+from dotenv import load_dotenv
+import os
+from pydantic_settings import BaseSettings
 
+load_dotenv()
+
+class Settings(BaseSettings):
+    EVOLUTION_API_URL: str = os.getenv("EVOLUTION_API_URL")
+    EVOLUTION_API_KEY: str = os.getenv("EVOLUTION_API_KEY")
+    EVOLUTION_INSTANCE: str = os.getenv("EVOLUTION_INSTANCE")
+    GROK_API_KEY: str = os.getenv("GROK_API_KEY")
+    REDIS_URL: str = os.getenv("REDIS_URL")
+    PORT: int = int(os.getenv("PORT", 3000))
+
+settings = Settings()
